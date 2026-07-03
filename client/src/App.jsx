878379e99e6af002e864c5410f6ed7d3c15f505b
developeranass <developeranass>
2026-07-03 11:11:42 +0500

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  
+  const [message, setMessage] = useState("");
+  
+  useEffect(()=> {
+    fetch("http://localhost:4000/api/message")
+    .then((res) => res.json())
+    .then((data) => setMessage(data.message));
+  
+  }, []);
+
+  return (
+  <>
+      <h1>Anas Sartaj</h1>
+      <h2>data  {message} </h2>
+    </>
+  )
+}
+
+export default App
